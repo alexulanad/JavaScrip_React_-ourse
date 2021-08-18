74e24c8b834acde79ec4ecb47d8fe098d8658ac6
AlexuL <alexulanad@gmail.com>
2021-08-18 23:35:27 +0300

    Первый коммит

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const category = "toys";
+console.log(`https://toyshops/${category}/15`); // чтобы добавить интерполяцию, следует строку обернуть в тип ковычек backtick `` (ё)
+
+const user = prompt("Как вас зовут?", "");
+alert(`Здравствуйте, ${user}!`);
