a539226d826a7b454588f922abe4125995690a9c
AlexuL <alexulanad@gmail.com>
2021-08-19 15:59:09 +0300

    Программа учета просмотренных фильмов :(добавлен функционал о количестве просмотренных фильмах и их рейтинге)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
-const category = "toys";
-console.log(`https://toyshops/${category}/15`); // чтобы добавить интерполяцию, следует строку обернуть в тип ковычек backtick `` (ё)
+'use strict'; // используем строгий режим кодинга
 
-const user = prompt("Как вас зовут?", "");
-alert(`Здравствуйте, ${user}!`);
+// запрашиваем данные от пользователя о количестве просмотренных фильмов
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "0");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const lastFilm = prompt("Ваш последний просмотренный фильм?", "Пусто"),
+      ratingFilm = +prompt("Оцените этот фильм от 0.0 до 10.0:", "10.0" ),
+      lastFilm2 = prompt("Ваш последний просмотренный фильм?", "Пусто"),
+      ratingFilm2 = +prompt("Оцените этот фильм от 0.0 до 10.0:", "10.0" );
+
+// personalMovieDB.movies[lastFilm] = ratingFilm;
+// personalMovieDB.movies[lastFilm2] = ratingFilm2;
+
+console.log(personalMovieDB);
+
+
+
