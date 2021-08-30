71ac3456370ca6a2dea65e0efc4f4fdd6f109a65
AlexuL <alexulanad@gmail.com>
2021-08-30 19:01:31 +0300

    Добавлены комментарии к блокам кода

--- js/script.js
+++ js/script.js
@@ -1,25 +1,51 @@
 'use strict'; // используем строгий режим кодинга
 
 // запрашиваем данные от пользователя о количестве просмотренных фильмов
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "0");
+let numberOfFilms; // хранит значение о количестве просмотренных фильмов
+let lastFilm; // временая переменая цикла, каждую итерацию хранит названия просмотренного фильма
+let ratingFilm; // временная переменная цикла, каждую итерацию хранит значение рейтингка фильма
 
+do {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "0");
+} while (numberOfFilms == "" || numberOfFilms <= 0);
+
+// Главная переменная, выполняющая роль базы данных о просмотренных фильмах
 const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
+    count: numberOfFilms, // количество просмотренных фильмов
+    movies: {}, // коллекция фильмов с их названиями, в качестве ключа и рейтингом, в качестве значения
+    actors: {}, 
     genres: [],
     privat: false
 };
 
-const lastFilm = prompt("Ваш последний просмотренный фильм?", "Пусто"),
-      ratingFilm = +prompt("Оцените этот фильм от 0.0 до 10.0:", "10.0" ),
-      lastFilm2 = prompt("Ваш последний просмотренный фильм?", "Пусто"),
-      ratingFilm2 = +prompt("Оцените этот фильм от 0.0 до 10.0:", "10.0" );
-
-// personalMovieDB.movies[lastFilm] = ratingFilm;
-// personalMovieDB.movies[lastFilm2] = ratingFilm2;
-
+for (let i = 0; i < personalMovieDB.count; i++) {
+    do {
+        lastFilm = prompt("Введите название просмотренного фильма:", "");
+        if (lastFilm == null) {
+            alert("Вы нажали отмена!");
+        } else if (lastFilm == "") {
+            alert("Вы ничего не ввели!");
+        } else if (lastFilm.length >= 50) {
+            alert("Название фильма слишком длинное (больше 50 символов)");
+        }
+    } while (lastFilm == null || lastFilm == "" || lastFilm.length >= 50);    
+    
+    do {
+        ratingFilm = +prompt("Укажите рейтинг фильма от 1.0 до 10:", "");
+        if (ratingFilm > 10.0) {
+            alert("Вы указали рейтинг фильма больше 10!");
+        } else if (ratingFilm < 1) {
+            alert("Вы указали рейтинг фильма менше 1.0 или не указали ничего!");
+        }
+    } while (ratingFilm == null || ratingFilm > 10.0 || ratingFilm < 1);
+    personalMovieDB.movies[lastFilm] = ratingFilm;
+}
+// Вердикт по количеству просмотренных фильмов
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else {
+    console.log("Вы киноман");
+}
 console.log(personalMovieDB);
-
-
-
